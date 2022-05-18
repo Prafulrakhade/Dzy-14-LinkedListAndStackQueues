2ae3061596a05e4af38b01836e1d86cb2b02fc15
Praful <prafulrakhade02@gmail.com>
2022-05-18 12:25:16 +0530

    [praful] Added : simple linkedlist program file

---
+++ LinkedList.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListProblem
+{
+    internal class LinkedList
+    {
+        internal Node head;
+        public void Add(int data)
+        {
+            Node n = new Node(data);
+            if(head==null)
+            {
+                head = n;
+                Console.WriteLine("{0} inserted into the linkedList", n.data);
+            }
+            else
+            {
+                n.next = head;
+                head = n;
+                Console.WriteLine("{0} inserted into the linkedlist", n.data);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace LinkedListProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("---------------------WELCOME TO THE LINKEDLIST  PROGRAM--------------------------\n\n");
+            LinkedList list = new LinkedList();
+            list.Add(56);
+            list.Add(30);
+            list.Add(70);
+        }
+    }
+}
